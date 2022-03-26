077d609da1dc4dc84f7bdd0b11c11067eb400ddf
Maker of Dreams <71730231+makerofdreams@users.noreply.github.com>
2022-03-26 13:32:34 +0530

    Update script.js

--- src/script.js
+++ src/script.js
@@ -1,3 +1,14 @@
+// preloader 
+document.onreadystatechange = function () {
+    if (document.readyState !== "complete") {
+        document.querySelector("body").style.visibility = "hidden";
+        document.querySelector("#loader").style.visibility = "visible";
+    } else {
+        document.querySelector("#loader").style.display = "none";
+        document.querySelector("body").style.visibility = "visible";
+    }
+};
+
 import './style.css'
 import * as dat from 'dat.gui'
 import * as THREE from 'three'
